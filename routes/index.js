5023b36ff3a3fbec69a7120a9b8c5f20a3d8ef90
Frank Su <jatongsu2003@gmail.com>
2020-01-04 10:20:04 -0500

    username chage

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,7 @@ router.get("/users/:id", middleware.checkUser, (req, res)=>{
             var validQuestions = []
             Question.find({}, function(err, questions){
                 if (err){
-                    console.log(err)
+                    console.log(err);
                 } else {
                     questions.forEach(function(q){
                         if (q.author.id.equals(user._id)){
@@ -35,22 +35,60 @@ router.get("/users/:id", middleware.checkUser, (req, res)=>{
     });
 });
 
+router.post("/users/:id/changeUsername", middleware.checkUser, (req, res)=>{
+    User.findById(mongoose.Types.ObjectId(req.params.id), function(err, user){
+        if (err){
+            console.log(err);
+            res.redirect("back");
+        } else {
+            User.find({}, function(err, users){
+                var stop = false;
+                users.forEach(function(use){
+                    if (use.username === req.body.usernameChange){
+                        stop = true;
+                    }
+                });
+                var noSpace = req.body.usernameChange.replace(/ /g, "");
+                if (noSpace.length < 1){
+                    stop = true;
+                }
+                if (stop){
+                    req.flash("error", "Username Already Taken");
+                    res.redirect("back");
+                } else {
+                    user.username = req.body.usernameChange;
+                    user.save();
+                    req.flash("success", "Username Chagned");
+                    res.redirect("back");
+                }
+            });
+        }
+    });
+});
+
 //Authorization Routes
 router.get("/register", (req, res)=>{
     res.render("register.ejs");
 });
 
 router.post("/register", (req, res)=>{
-    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
-        if (err){
-            console.log(err);
-            req.flash("error", err.message);
-            return res.redirect("/register");
-        }
-        passport.authenticate("local")(req, res, function(){
-            res.redirect("/questions");
+    var noSpace = req.body.username.replace(/ /g, "");
+    if (noSpace.length > 0){
+        User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+            if (err){
+                console.log(err);
+                req.flash("error", err.message);
+                return res.redirect("/register");
+            }
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/questions");
+            });
         });
-    });
+    } else {
+        req.flash("error", "Invalid Username");
+        res.redirect("/register");
+    }
+    
 });
 
 router.get("/login", (req, res)=>{
